5d697568fe9f8bd53530befb18a2be0b87a8c50a
Udit Kundu <uditkundu19@gmail.com>
2026-03-21 11:15:06 +0530

    playing more with pointers

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-
-	custom_append "github.com/Udit8158/go-learning/05_custom_append"
 )
 
 func Change(arr []int) {
@@ -12,31 +10,53 @@ func Change(arr []int) {
 	fmt.Println("Array changed, ", arr)
 }
 
+type F struct {
+	name  string
+	email string
+}
+
+func (f *F) nameChange(newName string) {
+	fmt.Printf("Memory address of f Type F in nameChange method %p\n", f)
+	fmt.Println((*f))
+	f.name = newName
+}
+
 func main() {
 
-	s := []int{1, 2, 3, 4, 5} // this a way to define a slice
-	s2 := make([]int, 5, 6)   // len -> 5, cap -> 8 slice (but this is better ig, more control)
+	// s := []int{1, 2, 3, 4, 5} // this a way to define a slice
+	// s2 := make([]int, 5, 6)   // len -> 5, cap -> 8 slice (but this is better ig, more control)
 
-	// interesting
-	// s2 = s2[:6]
-	// s2[5] = 12
+	// // interesting
+	// // s2 = s2[:6]
+	// // s2[5] = 12
 
-	fmt.Println(custom_append.CustomAppend(s2, 12))
-	fmt.Printf("After custom append %p\n", &s2[0])
+	// fmt.Println(custom_append.CustomAppend(s2, 12))
+	// fmt.Printf("After custom append %p\n", &s2[0])
 
-	fmt.Println("Current cap", cap(s2))
-	s2 = append(s2, 100)
-	s2 = append(s2, 100)
-	s2 = append(s2, 100)
-	s2 = append(s2, 100)
-	s2 = append(s2, 100)
-	fmt.Println(s2, len(s2), cap(s2))
-	// Change(s)
+	// fmt.Println("Current cap", cap(s2))
+	// s2 = append(s2, 100)
+	// s2 = append(s2, 100)
+	// s2 = append(s2, 100)
+	// s2 = append(s2, 100)
+	// s2 = append(s2, 100)
+	// fmt.Println(s2, len(s2), cap(s2))
+	// // Change(s)
 
-	fmt.Println(cap(s)) // capacity -> 5
-	s = append(s, 6)
-	fmt.Println(cap(s)) // capacity -> 10
-	s = append(s, 7)
-	fmt.Println(cap(s)) // capacity -> 10
-	fmt.Println(s)
+	// fmt.Println(cap(s)) // capacity -> 5
+	// s = append(s, 6)
+	// fmt.Println(cap(s)) // capacity -> 10
+	// s = append(s, 7)
+	// fmt.Println(cap(s)) // capacity -> 10
+	// fmt.Println(s)
+
+	var friend F
+	fmt.Printf("Memory address of friend %p\n", &friend)
+
+	fmt.Printf("%v\n", friend)
+	friend.name = "i"
+	friend.email = "k"
+	fmt.Printf("%#v\n", friend)
+
+	friend.nameChange("udit")
+	fmt.Printf("%#v\n", friend)
 }
